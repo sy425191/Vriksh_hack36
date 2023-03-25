1f430f052015c34541684276235087bc2620ea31
saurabh yadav <sy425191@gmail.com>
2023-03-25 14:34:08 +0530

    CampainDetails added

--- vriksh/lib/screens/home/campainDetails.dart
+++ vriksh/lib/screens/home/campainDetails.dart
@@ -10,15 +10,44 @@ class CampainDetails extends StatefulWidget {
 }
 
 class _CampainDetailsState extends State<CampainDetails> {
+  int _loading = 0;
+
+  void initState() {
+    super.initState();
+    setState(() {
+      _loading = 0;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.white,
-          elevation: 0,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: const Text(
+          'Campaign Details',
+          style: TextStyle(
+            color: Colors.black,
+          ),
         ),
-        body: Container(
-          color: Colors.green,
-        ));
+        centerTitle: true,
+        // back button
+        leading: IconButton(
+          icon: const Icon(
+            Icons.arrow_back,
+            color: Colors.black,
+          ),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      body: _loading == 0
+          ? const Center(
+              child: CircularProgressIndicator(),
+            )
+          : Container(),
+    );
   }
 }
