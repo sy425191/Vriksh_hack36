c00442cb1ece09fe2b69c571bfb56f50fd4b9733
Devansh Srivastava <devanshsks@gmail.com>
2023-03-26 08:16:17 +0530

    homepage info

--- vriksh/lib/screens/home/homeApps.dart
+++ vriksh/lib/screens/home/homeApps.dart
@@ -27,12 +27,25 @@ class HomeAppsState extends State<HomeApps> {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Container(
-            height: 250,
+            height: 100,
+            width: double.infinity,
+            padding: EdgeInsets.symmetric(
+              horizontal: 10,
+            ),
             decoration: const BoxDecoration(
               color: Color.fromARGB(255, 194, 255, 184),
               borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(50),
-                bottomRight: Radius.circular(50),
+                bottomLeft: Radius.circular(20),
+                bottomRight: Radius.circular(20),
+              ),
+            ),
+            child: Center(
+              child: Text(
+                "Vriksh - Home",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ),
           ),
@@ -44,7 +57,109 @@ class HomeAppsState extends State<HomeApps> {
               horizontal: 10,
             ),
             child: Column(
-              children: [],
+              children: [
+                // show users the ways by which they can contribute to sustainability
+                SizedBox(
+                  height: 30,
+                ),
+
+                Text(
+                    "Vriksh is a analytics based app that helps you to track tree covers in your area and also helps you to contribute to the environment by planting trees."),
+
+                SizedBox(
+                  height: 30,
+                ),
+                Text(
+                  "How can you contribute?",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Column(
+                      children: [
+                        Container(
+                          height: 100,
+                          width: 120,
+                          decoration: BoxDecoration(
+                            color: Colors.green,
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          child: Center(
+                            child: Text(
+                              "Plant a tree",
+                              style: TextStyle(
+                                fontSize: 15,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Container(
+                          height: 100,
+                          width: 100,
+                          decoration: BoxDecoration(
+                            color: Colors.green,
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          child: Center(
+                            child: Text(
+                              "Join a drive",
+                              style: TextStyle(
+                                fontSize: 15,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Container(
+                          height: 100,
+                          width: 120,
+                          decoration: BoxDecoration(
+                            color: Colors.green,
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          child: Center(
+                            child: Text(
+                              "Conduct a drive",
+                              style: TextStyle(
+                                fontSize: 15,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
             ),
           )
         ],
