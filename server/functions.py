e49b272c0a02f69622409347eb441854bb709b3d
SAURABH YADAV <75905524+sy425191@users.noreply.github.com>
2023-10-15 14:58:57 +0530

    Update functions.py

--- server/functions.py
+++ server/functions.py
@@ -4,7 +4,8 @@ import json
 
 def get_weather_data(lat, long):
     weather_api_endpoint = "http://api.openweathermap.org/data/2.5/air_pollution"
-    weather_api_key = "ce48d24e380120ecc6703f73d38ac768"
+    # weather_api_key = "saura   ce48d24e380120ecc6703f73d38ac768"
+    weather_api_key = "VALID_API_KEY"
 
     weather_data_link = weather_api_endpoint + "?lat=" + str(lat) + "&lon=" + str(long) + "&appid=" + weather_api_key
     # print(weather_data_link)
